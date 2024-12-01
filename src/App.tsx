8fc558671a2355e6d8cd36b924440823c6c879ce
Andre <lourdandre@gmail.com>
2024-12-01 16:01:53 -0500

    add Authenticator

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,23 @@
-import { useState } from 'react'
+import { Amplify } from 'aws-amplify';
+import outputs from '../amplify_outputs.json';
+import { Authenticator } from '@aws-amplify/ui-react';
 import './App.css'
+import '@aws-amplify/ui-react/styles.css';
+Amplify.configure(outputs);
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <h1 className="text-2xl">Hello Amplify-React-Vite-Tailwind-DaisyUI!</h1><br/>
-      <ul className="menu bg-base-200 rounded-box w-56">
-        <li><a>Item 1</a></li>
-        <li><a>Item 2</a></li>
-        <li><a>Item 3</a></li>
-      </ul>
+      <div className="p-4">
+        <Authenticator>
+          <h1 className="text-2xl">Breakfast</h1><br />
+          <ul className="menu bg-base-200 rounded-box">
+            <li><a>Item 1</a></li>
+            <li><a>Item 2</a></li>
+            <li><a>Item 3</a></li>
+          </ul>
+        </Authenticator>
+      </div>
     </>
   )
 }
